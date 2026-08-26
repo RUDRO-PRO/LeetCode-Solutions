0da0e2920e55af5df0543d05810bf61858675fba
RUDRO CHAKRABORTY <rudrochakraborty28@gmail.com>
2026-08-26 18:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (56.39%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int n = height.size();
+        if (n <= 2) return 0;
+
+        int left = 0;
+        int right = n - 1;
+        int left_max = 0;
+        int right_max = 0;
+        int water_trapped = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    water_trapped += (left_max - height[left]);
+                }
+                left++;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    water_trapped += (right_max - height[right]);
+                }
+                right--;
+            }
+        }
+
+        return water_trapped;
+    }
+};
